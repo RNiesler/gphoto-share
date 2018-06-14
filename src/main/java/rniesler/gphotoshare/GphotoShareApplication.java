1f2ff398079a870e95c314d5e3783a321f60042b
Rafal Niesler <rniesler@gmail.com>
2018-06-14 16:24:07 +0200

    Spring-boot generated project

---
+++ src/main/java/rniesler/gphotoshare/GphotoShareApplication.java
@@ -0,0 +1,12 @@
+package rniesler.gphotoshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GphotoShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GphotoShareApplication.class, args);
+	}
+}
