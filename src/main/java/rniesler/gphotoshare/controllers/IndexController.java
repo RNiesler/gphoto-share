6bde4a8cd15cfa8fb4964b2cfe87a1eefe671442
Rafal Niesler <rniesler@gmail.com>
2018-06-14 21:01:50 +0200

    Spring security tutorial

---
+++ src/main/java/rniesler/gphotoshare/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package rniesler.gphotoshare.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
