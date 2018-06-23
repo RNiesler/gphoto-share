567fa1e4aab5af775a9b517b210ec8c0a40e8b81
Rafal Niesler <rniesler@gmail.com>
2018-06-23 09:10:49 +0200

    Use SimpleKeyGenerator to generate params part of the cache key

--- src/main/java/rniesler/gphotoshare/GphotoShareApplication.java
+++ src/main/java/rniesler/gphotoshare/GphotoShareApplication.java
@@ -4,12 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.cache.interceptor.SimpleKeyGenerator;
 import org.springframework.context.annotation.Bean;
 import rniesler.gphotoshare.security.SecurityService;
-
-import java.util.Arrays;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @EnableCaching
 @SpringBootApplication
@@ -27,15 +24,7 @@ public class GphotoShareApplication {
             keyBuilder.append("-");
             keyBuilder.append(securityService.getAuthenticatedEmail());
             keyBuilder.append("-");
-            keyBuilder.append(Arrays.stream(params).map(obj -> {
-                if (obj == null) {
-                    return "null";
-                } else if (obj instanceof Optional) {
-                    return ((Optional) obj).isPresent() ? ((Optional) obj).get().toString() : "empty";
-                } else {
-                    return obj.toString();
-                }
-            }).collect(Collectors.joining(",")));
+            keyBuilder.append(SimpleKeyGenerator.generateKey(params));
             return keyBuilder.toString();
         };
     }
